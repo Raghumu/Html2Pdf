c1908850191866e20c72d2fafb0fe9e731ffe034
Raghumu <raghumu444@gmail.com>
2021-07-25 22:32:59 +0530

    Initial Commit
    This project Converts HTML to PDF using Rotativa nuget package.

---
+++ Html2Pdf/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Html2Pdf.Models;
+using Microsoft.AspNetCore.Mvc;
+using Rotativa.AspNetCore;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Html2Pdf.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult PrintIndexPage()
+        {
+            return new ViewAsPdf("~/Views/Home/Index.cshtml");
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
